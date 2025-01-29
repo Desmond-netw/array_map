8b6737000028a9feaf962fb5d7a74c614506e003
Desmond-netw <dsquare200@gmail.com>
2025-01-29 09:41:38 +0200

    array map -example 5

---
+++ conver-arr-uppercase.js
@@ -0,0 +1,7 @@
+// converting element of an array to uppercase
+
+let names = ["John", "Bubu", "alice", "rice"];
+
+const uppercaseNames = names.map(name => name.toUpperCase());
+
+console.log (uppercaseNames);
