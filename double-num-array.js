c5c2bd8b2bd23cbefafabdd1976e4203fae339fe
Desmond-netw <dsquare200@gmail.com>
2025-01-29 08:46:36 +0200

    array-map example 1

---
+++ double-num-array.js
@@ -0,0 +1,8 @@
+
+// Double each number in this array
+
+const mynumbers = [5,6,7,8,9,10];
+const doubleNumbers = mynumbers.map(numb => numb * 2);
+
+console.log (mynumbers);
+console.log(doubleNumbers)
