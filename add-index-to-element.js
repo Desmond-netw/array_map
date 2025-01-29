2fc02df96541ee66364dac2c29571c4e7f549216
Desmond-netw <dsquare200@gmail.com>
2025-01-29 09:22:57 +0200

    array map example 3

---
+++ add-index-to-element.js
@@ -0,0 +1,6 @@
+// Add the index of an element to that element to incrase it value
+
+const numbers = [20, 40, 50, 61, 72];
+
+const indexedNumbers = numbers.map((num, index) => num + index);
+console.log(indexedNumbers)
